9f40e0c6e02373d1dc328c357a0b05356d58cfa4
Garrett Battaglia <garrett@garrettbattaglia.info>
2017-11-28 00:23:26 -0600

    fixed issue with missing aclocal
    when running with just autoconf installed autoremake was missing aclocal

--- Formula/autoconf.rb
+++ Formula/autoconf.rb
@@ -5,6 +5,8 @@ class Autoconf < Formula
   mirror "https://ftpmirror.gnu.org/autoconf/autoconf-2.69.tar.gz"
   sha256 "954bd69b391edc12d6a4a51a2dd1476543da5c6bbf05a95b59dc0dd6fd4c2969"
 
+  depends_on "automake" => :run
+  
   bottle do
     cellar :any_skip_relocation
     rebuild 4
